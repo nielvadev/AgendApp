b23e78e09b6b6c5389fd06a351f296028b7389e9
Juan Daniel Valencia <nielvadev@gmail.com>
2022-11-30 11:19:12 -0500

    Finalizado v1

--- AgendApp/View/About.xaml.cs
+++ AgendApp/View/About.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,22 @@ namespace AgendApp.View
         public About()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var url = button.ClassId;
+
+            await Browser.OpenAsync(url);
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var url = button.ClassId;
+
+            await Browser.OpenAsync(url);
         }
     }
 }
